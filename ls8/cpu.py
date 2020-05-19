80e322acb1974620f54326a670b694125351628c
nicanor-alicaya <donalicaya@gmail.com>
2020-05-19 00:24:35 -0400

    day 1 done

--- ls8/cpu.py
+++ ls8/cpu.py
@@ -7,7 +7,10 @@ class CPU:
 
     def __init__(self):
         """Construct a new CPU."""
-        pass
+        self.ram = [0] * 256
+        self.reg = [0] * 8
+        self.pc = 0
+      
 
     def load(self):
         """Load a program into memory."""
@@ -29,6 +32,12 @@ class CPU:
         for instruction in program:
             self.ram[address] = instruction
             address += 1
+    
+    def ram_read(self, MAR):
+        return self.ram[MAR]
+
+    def ram_write(self,MAR, MDR):
+        self.ram[MAR] = MDR    
 
 
     def alu(self, op, reg_a, reg_b):
@@ -62,4 +71,30 @@ class CPU:
 
     def run(self):
         """Run the CPU."""
-        pass
+
+        LDI= 0b10000010
+        PRN= 0b01000111
+        HLT= 0b00000001
+
+        halted = False
+
+        while not halted:
+            instruction = self.ram_read(self.pc)
+            opr_a = self.ram_read(self.pc + 1)
+            opr_b = self.ram_read(self.pc + 2)
+
+            if instruction == LDI:
+                self.reg[opr_a] = opr_b
+                self.pc += 3
+
+            elif instruction == PRN:
+                print(self.reg[opr_a])
+                self.pc += 2
+
+            elif instruction == HLT:
+                self.pc += 1
+                halted = True
+
+            else:
+                print(f'unknown instruction {instruction} at address {pc}')
+                sys.exit(1)         
